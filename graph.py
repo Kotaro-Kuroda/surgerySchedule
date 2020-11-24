3c351cb4ef3834dd289559308da9d56f8dfed0b4
Kotaro Kuroda <kuroda.k.ak@m.titech.ac.jp>
2020-11-24 14:52:41 +0900

    保存場所を指定

--- graph.py
+++ graph.py
@@ -8,8 +8,10 @@ import pandas as pd
 
 # -*- coding: utf-8 -*-
 
+image_path = "/Users/kurodakotaro/Documents/image/"
 
-def create_ganttchart(url):
+
+def create_ganttchart(url, save_path):
     mpl.rcParams['font.family'] = "IPAexGothic"
     df = pd.ExcelFile(url)
     shiftLength = 480
@@ -17,6 +19,8 @@ def create_ganttchart(url):
         excel = pd.read_excel(
             url,
             sheet_name=sheet, header=0, index_col=0)
+        if len(excel) <= 1:
+            continue
         ind = excel.columns
         label_x = []
         plt.figure(figsize=(30, 10), dpi=50)
@@ -24,8 +28,7 @@ def create_ganttchart(url):
         for i in range(0, 96):
             hour = int((i * 5) / 60 + 9)
             minute = (i * 5) % 60
-            time = ""
-            if (minute < 10):
+            if minute < 10:
                 time = str(hour) + ":" + "0" + str(minute)
             else:
                 time = str(hour) + ":" + str(minute)
@@ -33,39 +36,39 @@ def create_ganttchart(url):
         k = 0
         l = 0
         m = 0
+        btm = 0
         for i in range(0, len(excel)):
             data = excel.iloc[i, :]
             btm = 0
             for j in range(0, i):
                 btm += np.array(excel.iloc[j, :].values)
-            if (excel.index[i] == "空白"):
+            if excel.index[i] == "空白":
                 plt.barh(
                     ind, data, height=0.8, left=btm, align='center',
                     color="white")
             else:
-                if ("準備" in excel.index[i]):
+                if "準備" in excel.index[i]:
                     if k == 0:
                         plt.barh(ind, data, height=0.8, left=btm, align='center', color="red", label="preparation")
                         k += 1
                     else:
                         plt.barh(ind, data, height=0.8, left=btm, align='center', color="red")
-                if("手術" in excel.index[i]):
+                if "手術" in excel.index[i]:
                     if l == 0:
                         plt.barh(ind, data, height=0.8, left=btm, align='center', color="blue", label="surgery")
                         l += 1
                     else:
                         plt.barh(ind, data, height=0.8, left=btm, align='center', color="blue")
-                if ("清掃" in excel.index[i]):
+                if "清掃" in excel.index[i]:
                     if m == 0:
                         plt.barh(ind, data, height=0.8, left=btm, align='center', color="yellow", label="cleaning")
                         m += 1
                     else:
                         plt.barh(ind, data, height=0.8, left=btm, align='center', color="yellow")
         plt.grid(axis="x", alpha=0.8)
-        plt.title("手術スケジュール", fontsize=36)
+        plt.title("手術スケジュール" + sheet, fontsize=36)
         timeList = []
-        time = ""
-        for i in range(0, max(480, max(btm)) + 30, 30):
+        for i in range(0, max(shiftLength, round(max(btm))) + 30, 30):
             hour = int(i / 60) + 9
             minute = i % 60
             if minute < 10:
@@ -74,13 +77,13 @@ def create_ganttchart(url):
                 time = str(hour) + ":" + str(minute)
             timeList.append(time)
         plt.tick_params(labelsize=18)
-        plt.xticks(range(0, max(shiftLength, max(btm)) + 30, 30), timeList)
+        plt.xticks(range(0, max(shiftLength, round(max(btm))) + 30, 30), timeList)
         plt.legend(bbox_to_anchor=(1.06, 0), loc='lower right', borderaxespad=0)
-        plt.savefig(sheet + ".png")
+        plt.savefig(save_path + sheet + ".png")
     plt.show()
 
 
-def create_surgeon_gantt_chart(url):
+def create_surgeon_ganttchart(url, save_path):
     mpl.rcParams['font.family'] = "IPAexGothic"
     shiftLength = 480
     df = pd.ExcelFile(url)
@@ -88,6 +91,8 @@ def create_surgeon_gantt_chart(url):
         excel = pd.read_excel(
             url,
             sheet_name=sheet, header=0, index_col=0)
+        if len(excel) <= 1:
+            continue
         ind = excel.columns
         label_x = []
         plt.figure(figsize=(50, 10), dpi=50)
@@ -96,12 +101,13 @@ def create_surgeon_gantt_chart(url):
             hour = int((i * 5) / 60 + 9)
             minute = (i * 5) % 60
             time = ""
-            if (minute < 10):
+            if minute < 10:
                 time = str(hour) + ":" + "0" + str(minute)
             else:
                 time = str(hour) + ":" + str(minute)
             label_x.append(time)
         l = 0
+        btm = 0
         for i in range(0, len(excel)):
             data = excel.iloc[i, :]
             btm = 0
@@ -122,7 +128,7 @@ def create_surgeon_gantt_chart(url):
         plt.legend(bbox_to_anchor=(1.06, 0), loc='lower right', borderaxespad=0)
         timeList = []
         time = ""
-        for i in range(0, max(480, max(btm)) + 30, 30):
+        for i in range(0, max(480, round((max(btm)))) + 30, 30):
             hour = int(i / 60) + 9
             minute = i % 60
             if minute < 10:
@@ -131,6 +137,6 @@ def create_surgeon_gantt_chart(url):
                 time = str(hour) + ":" + str(minute)
             timeList.append(time)
         plt.tick_params(labelsize=18)
-        plt.xticks(range(0, max(shiftLength, max(btm)) + 30, 30), timeList)
-        plt.savefig(sheet + ".png")
+        plt.xticks(range(0, max(shiftLength, round(max(btm))) + 30, 30), timeList)
+        plt.savefig(save_path + sheet + ".png")
     plt.show()
